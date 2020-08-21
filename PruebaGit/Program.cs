012b907a342ae8730f7cbfd2cd08e23fd7819fce
Rodrigo <rmoreiras@timesolution.com.ar>
2020-08-21 15:28:35 -0300

    agrego cambio

--- PruebaGit/Program.cs
+++ PruebaGit/Program.cs
@@ -11,6 +11,7 @@ namespace PruebaGit
         static void Main(string[] args)
         {
             Console.WriteLine("Hola mundo Rodrigo");
+            Console.WriteLine("Nuevo cambio Rodrigo");
             Console.ReadKey();
         }
     }
